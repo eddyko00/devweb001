ff0bd9ea7589dda6e163cb22ed15cdb3c46b5e9e
koed <eddy.ko00@yahoo.ca>
2020-03-14 12:23:43 -0400

    graph

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 echo "cordova platform add android"
-cordova platform add android
+call cordova1.bat
+
 :choice1
 set /P c=Want to continue[Y/N]?
 if /I "%c%" EQU "Y" goto :cont1
@@ -10,7 +11,8 @@ goto :choice1
 :cont1
 
 echo "cordova requirements"
-cordova requirements
+call cordova2.bat
+
 
 :choice2
 set /P c=Want to continue[Y/N]?
@@ -20,7 +22,8 @@ goto :choice2
 
 :cont2
 echo "cordova build"
-cordova build
+call cordova3.bat
+
 
 :choice3
 set /P c=Want to continue[Y/N]?
@@ -30,7 +33,8 @@ goto :choice2
 
 :cont2
 echo "cordova run android"
-cordova run android
+call cordova4.bat
+
 
 pause
 exit
