1e887d73bee3ab2106d057d7f32ae0f66c3237f5
koed <eddy.ko00@yahoo.ca>
2020-03-16 12:52:19 -0400

    graph

--- www/js/iisweb.js
+++ www/js/iisweb.js
@@ -1,12 +1,25 @@
 
 var app = {
-    // Application Constructor
-    initialize: function () {
-        var iisurl = "https://iiswebsrv.herokuapp.com/";
 
-        $("#loadImage").click(function () {
+// Application Constructor
+    initialize: function () {
+
+        $(document).ready(function () {
+            // after the page elements are all loaded, then run the script
+            // Set the input field with unique ID #email to a value
+            $("#txt-email").val('GUEST');
+            // Set the input field with unique ID #name
+            $("#txt-password").val('guest');
+
+        });
+
+
+        var iisurl = "https://iiswebsrv.herokuapp.com/";
+        $("#btn-login").click(function () {
+            var txemail = document.getElementById("txt-email").value;
+            var txtpassword = document.getElementById("txt-password").value;
             $.ajax({
-                url: iisurl + "cust/login?email=guest&pass=guest",
+                url: iisurl + "cust/login?email=" + txemail + "&pass=" + txtpassword,
                 crossDomain: true,
                 cache: false,
                 success: handleResult
@@ -15,87 +28,77 @@ var app = {
             // add cordova progress indicator https://www.npmjs.com/package/cordova-plugin-progress-indicator
 
             function handleResult(result) {
-                $("#desc").text(result.webMsg.result);
+                console.log(result.custObj)
+                var custObj = result.custObj;
+                var jsonStr = JSON.stringify(result, null, '\t');
+
+                if (custObj != null) {
+                    window.location.href = "#page-signup-succeeded";
+                } else {
+                    $('#error_message').fadeIn().html(jsonStr);
+                }
             }
         });
 
-        $("#randomImage").click(function () {
-            $.ajax({
-                url: url + "&date=" + randomDate(new Date(2015, 0, 1), new Date()),
-                success: handleResult
-            }); // use promises
-
-            // add cordova progress indicator https://www.npmjs.com/package/cordova-plugin-progress-indicator
-
-            function handleResult(result) {
-                $("#spaceimage").attr("src", result.url);
-
-                //http://responsiveimg.com/
-                $("#spaceimage").responsiveImg();
-
-                $("#copyright").text("Copyright: " + result.copyright);
-                $("#desc").text(result.explanation);
-            }
-        });
-
+// example        
+//alert("AJAX request successfully completed");
+//var jsonObj = JSON.parse(jsonStr);
+//var jsonPretty = JSON.stringify(jsonObj, null, '\t');
 
     },
 };
-
 app.initialize();
-
-
-function checkForm() {
-    var iisurl = "https://iiswebsrv.herokuapp.com/";
-    var result = true;
-
-    $(".errorMsg").remove();
-
-    var userId = document.getElementById("userId").value;
-    var password = document.getElementById("password").value;
-    if (userId.length == 0) {
-        userId = "guest";
-        password = "guest";
-    }
-
-
-    $("input[type='password']").each(function (idx) {
-        if (this.value.length == 0) {
-
-            $("#messageList").append(
-                    "<li class='errorMsg'>Input can not be empty: " + this.name
-                    + "</li>");
-
-            result = false;
-            $(this).addClass("inputErr");
-        } else {
-            $(this).removeClass("inputErr");
-        }
-    });
-
-
-    if (result == false) {
-        $("#messages").show();
-        return result;
-    } else {
-        $("#messages").hide();
-    }
-
-    $.ajax({
-        url: iisurl + "cust/login?email=" + userId + "&pass=" + password,
-        crossDomain: true,
-        cache: false,
-        success: handleResult
-    }); // use promises
-
-    // add cordova progress indicator https://www.npmjs.com/package/cordova-plugin-progress-indicator
-
-    function handleResult(result) {
-        $("#desc").text(result.webMsg.result);
-    }
-
-    return result;
-}
+//function checkForm() {
+//    var iisurl = "https://iiswebsrv.herokuapp.com/";
+//    var result = true;
+//
+//    $(".errorMsg").remove();
+//
+//    var userId = document.getElementById("userId").value;
+//    var password = document.getElementById("password").value;
+//    if (userId.length == 0) {
+//        userId = "guest";
+//        password = "guest";
+//    }
+//
+//
+//    $("input[type='password']").each(function (idx) {
+//        if (this.value.length == 0) {
+//
+//            $("#messageList").append(
+//                    "<li class='errorMsg'>Input can not be empty: " + this.name
+//                    + "</li>");
+//
+//            result = false;
+//            $(this).addClass("inputErr");
+//        } else {
+//            $(this).removeClass("inputErr");
+//        }
+//    });
+//
+//
+//    if (result == false) {
+//        $("#messages").show();
+//        return result;
+//    } else {
+//        $("#messages").hide();
+//    }
+//
+//    $.ajax({
+//        url: iisurl + "cust/login?email=" + userId + "&pass=" + password,
+//        crossDomain: true,
+//        cache: false,
+//        success: handleResult
+//    }); // use promises
+//
+//    // add cordova progress indicator https://www.npmjs.com/package/cordova-plugin-progress-indicator
+//
+//    function handleResult(result) {
+//        $("#desc").text(result.webMsg.result);
+//    }
+//
+//    return result;
+//}
 
 
 
