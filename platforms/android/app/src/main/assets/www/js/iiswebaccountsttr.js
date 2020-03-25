9db285d2a48d360451cab42eed36049d931e998a
koed <eddy.ko00@yahoo.ca>
2020-03-25 18:09:13 -0400

    graph

--- platforms/android/app/src/main/assets/www/js/iiswebaccountsttr.js
+++ platforms/android/app/src/main/assets/www/js/iiswebaccountsttr.js
@@ -176,7 +176,7 @@ var app = {
                             if (resultPerfList !== null) {
                                 var PerfObj = resultPerfList[0];
                                 var htmlName = '<div class="ui-grid-a">';
-                                htmlName += '<div class="ui-block-a" ><strong>' + PerfObj.updatedatedisplay + '</strong></div>';
+                                htmlName += '<div class="ui-block-a" ><strong>' + PerfObj.updatedatedisplay + '</strong> - 1 Yr performance result</div>';
                                 htmlName += '<div class="ui-block-b" >' + " " + '</div>';
                                 htmlName += '</div>';
 
